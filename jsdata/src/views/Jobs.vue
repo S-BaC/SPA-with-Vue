7fcb69fb697f95bd06379843284e4b4b1254cc42
ba_c <swunhtetoo@gmail.com>
2022-06-14 16:07:33 +0630

    Using fetch API

---
+++ jsdata/src/views/Jobs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-for="job in jobs">
+    <h2>{{job.age}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            jobs:[]
+        }
+    },
+    mounted(){
+        fetch('http://localhost:3000/jobs')
+            .then(res => res.json())
+            .then(data => this.jobs = data)
+            .catch(err => console.log(err.message));
+    }
+}
+</script>
+
+<style>
+
+</style>
